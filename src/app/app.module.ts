9fd1fb44d5a4154ff63de9cd3f8ea235aeb781cf
Aschaley Ascheneke <45295709+aschalegn@users.noreply.github.com>
2020-03-04 14:58:34 +0200

    style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,23 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HistoryComponent } from './history/history.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { HomeComponent } from './home/home.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module'
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HistoryComponent,
     MovieDetailComponent,
-    HomeComponent
+    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
